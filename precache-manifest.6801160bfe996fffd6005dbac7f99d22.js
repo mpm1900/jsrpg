319f5ca9f0d2580ee44b2ce4d368c140e0a8fd2a
Max Miller <mpm1900@users.noreply.github.com>
2020-08-30 00:35:55 -0500

    Updates

---
+++ precache-manifest.6801160bfe996fffd6005dbac7f99d22.js
@@ -0,0 +1,274 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909820ee4139a9a102cef1cc6e0b3ce0",
+    "url": "/jsrpg/index.html"
+  },
+  {
+    "revision": "bfc5d49452c5090f330c",
+    "url": "/jsrpg/static/css/main.6b640b01.chunk.css"
+  },
+  {
+    "revision": "d7315a718e8e5735de08",
+    "url": "/jsrpg/static/js/2.5bd51865.chunk.js"
+  },
+  {
+    "revision": "4fc96d8633d093f6effd8ad2d7e6794c",
+    "url": "/jsrpg/static/js/2.5bd51865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc5d49452c5090f330c",
+    "url": "/jsrpg/static/js/main.5c3eb843.chunk.js"
+  },
+  {
+    "revision": "c455a9ffbc4cbbcd5a32",
+    "url": "/jsrpg/static/js/runtime-main.569d6e6e.js"
+  },
+  {
+    "revision": "7247592441e4d352a7757cdf23749b5b",
+    "url": "/jsrpg/static/media/aura.72475924.svg"
+  },
+  {
+    "revision": "c6c3f6ae570767a3f1d0eb18d1eebd97",
+    "url": "/jsrpg/static/media/barbed-arrow.c6c3f6ae.svg"
+  },
+  {
+    "revision": "e93dc754c0154fb86dc02c5905208e20",
+    "url": "/jsrpg/static/media/barbute.e93dc754.svg"
+  },
+  {
+    "revision": "12de19c649cff2210e11a72790610937",
+    "url": "/jsrpg/static/media/bleeding-eye.12de19c6.svg"
+  },
+  {
+    "revision": "59d8ca0f9d928923fe1963d5e161f221",
+    "url": "/jsrpg/static/media/bloody-sword.59d8ca0f.svg"
+  },
+  {
+    "revision": "a02eb2a54ff9b7a10e130bde24ec5d45",
+    "url": "/jsrpg/static/media/book-cover.a02eb2a5.svg"
+  },
+  {
+    "revision": "b6910f109fa09a4a332fe54ce40aab0c",
+    "url": "/jsrpg/static/media/boots.b6910f10.svg"
+  },
+  {
+    "revision": "11d3824512bbe74b88a6d8eecdc329e5",
+    "url": "/jsrpg/static/media/charge.11d38245.png"
+  },
+  {
+    "revision": "59102a4b750ac54602e432f59ca2e086",
+    "url": "/jsrpg/static/media/closed-barbute.59102a4b.svg"
+  },
+  {
+    "revision": "f9be904839f433ca0e9c3278972b93f0",
+    "url": "/jsrpg/static/media/crippling-blow.f9be9048.png"
+  },
+  {
+    "revision": "dfa5ecf64ee14516cf0a7570e8f7a051",
+    "url": "/jsrpg/static/media/croc-sword.dfa5ecf6.svg"
+  },
+  {
+    "revision": "6d23d5442ead4c9dcb37524087b3fa4c",
+    "url": "/jsrpg/static/media/crossbow.6d23d544.svg"
+  },
+  {
+    "revision": "b7d2c27aa5e3c339407d1442370fe343",
+    "url": "/jsrpg/static/media/crossed-swords.b7d2c27a.svg"
+  },
+  {
+    "revision": "155cdf6ec5eeff616b03ed44886589be",
+    "url": "/jsrpg/static/media/dark-blast.155cdf6e.png"
+  },
+  {
+    "revision": "e202d07a30c7bba9a9c30a4835c7332e",
+    "url": "/jsrpg/static/media/dice-six-faces-six.e202d07a.svg"
+  },
+  {
+    "revision": "3106dbaefae22145f91856d0e6f259b4",
+    "url": "/jsrpg/static/media/fire-axe.3106dbae.svg"
+  },
+  {
+    "revision": "267cc34c9400b0b24461e6796c02beb1",
+    "url": "/jsrpg/static/media/fireball.267cc34c.png"
+  },
+  {
+    "revision": "d1b25a44a4075fbe3506e817eb7c9fc1",
+    "url": "/jsrpg/static/media/fist.d1b25a44.svg"
+  },
+  {
+    "revision": "1f360b03b64a102eaf01c3f079824d16",
+    "url": "/jsrpg/static/media/flail.1f360b03.svg"
+  },
+  {
+    "revision": "332be8c2105fc22111b2f94ee0e6c30a",
+    "url": "/jsrpg/static/media/flame.332be8c2.svg"
+  },
+  {
+    "revision": "88018ef0a57f5db67b5de3002de5f277",
+    "url": "/jsrpg/static/media/footprint.88018ef0.svg"
+  },
+  {
+    "revision": "03da1411244855d352cb1db768ae5d10",
+    "url": "/jsrpg/static/media/gloves.03da1411.svg"
+  },
+  {
+    "revision": "9a950e426bfc8625620d1ba61b6a3cbe",
+    "url": "/jsrpg/static/media/hand.9a950e42.svg"
+  },
+  {
+    "revision": "6549c4b8439b43074ac067848e01664c",
+    "url": "/jsrpg/static/media/headshot.6549c4b8.png"
+  },
+  {
+    "revision": "e72c5d2c7d80ad5aa83c55c9338b7fa5",
+    "url": "/jsrpg/static/media/healing1.e72c5d2c.png"
+  },
+  {
+    "revision": "7b3df12775e099ecd441b7701ac1de8e",
+    "url": "/jsrpg/static/media/hood.7b3df127.svg"
+  },
+  {
+    "revision": "af959ce1061a3952ece76d4de408e32f",
+    "url": "/jsrpg/static/media/inspect.af959ce1.png"
+  },
+  {
+    "revision": "8ca51fab66f2ee3d68d7d7660f4ba50e",
+    "url": "/jsrpg/static/media/katana.8ca51fab.svg"
+  },
+  {
+    "revision": "ec54b497c43351f1868fc9a52865e3d0",
+    "url": "/jsrpg/static/media/luger.ec54b497.svg"
+  },
+  {
+    "revision": "ec7b5987365d700fd34426572fd8e91e",
+    "url": "/jsrpg/static/media/magnifying-glass.ec7b5987.svg"
+  },
+  {
+    "revision": "d014b37d885220c33eff32ac7089535c",
+    "url": "/jsrpg/static/media/moebius-trefoil.d014b37d.svg"
+  },
+  {
+    "revision": "92dbbc2a91681d788ac13c9179fd654a",
+    "url": "/jsrpg/static/media/muscular-torso.92dbbc2a.svg"
+  },
+  {
+    "revision": "2f0c673e0c1db37023a459d0b95fff3d",
+    "url": "/jsrpg/static/media/orb-wand.2f0c673e.svg"
+  },
+  {
+    "revision": "ac0b4bc7be145d36b9007d9c51496564",
+    "url": "/jsrpg/static/media/pencil.ac0b4bc7.svg"
+  },
+  {
+    "revision": "ed9dcd223aec5ae066b2a5099a8e9dcc",
+    "url": "/jsrpg/static/media/power-ring.ed9dcd22.svg"
+  },
+  {
+    "revision": "1d87b52fdcd08e9c112b36c6d2e8908e",
+    "url": "/jsrpg/static/media/quick-slash.1d87b52f.svg"
+  },
+  {
+    "revision": "5187be7254dacbd8c45841104a9593bf",
+    "url": "/jsrpg/static/media/ring.5187be72.svg"
+  },
+  {
+    "revision": "5b61ecba7d48947b3cea60e131678a07",
+    "url": "/jsrpg/static/media/robe.5b61ecba.svg"
+  },
+  {
+    "revision": "300ea409d58dc8cefc32ac6b46ae4bb5",
+    "url": "/jsrpg/static/media/roll.300ea409.png"
+  },
+  {
+    "revision": "ec2d8043dfdf0eb05ac389381450abdf",
+    "url": "/jsrpg/static/media/save.ec2d8043.svg"
+  },
+  {
+    "revision": "b9675e5bb25ef17432ec8eedf98da36f",
+    "url": "/jsrpg/static/media/shadow-strike.b9675e5b.png"
+  },
+  {
+    "revision": "23a923d518ede71041a607caa26cec91",
+    "url": "/jsrpg/static/media/shard-sword.23a923d5.svg"
+  },
+  {
+    "revision": "2cc62e8aab89a8d0d2bec981bb8e1667",
+    "url": "/jsrpg/static/media/shield.2cc62e8a.svg"
+  },
+  {
+    "revision": "caa17ee92040d31407446494e47a4536",
+    "url": "/jsrpg/static/media/shoulder-armor.caa17ee9.svg"
+  },
+  {
+    "revision": "60519e24d4fda8de942b1bfc78e94f8e",
+    "url": "/jsrpg/static/media/skills.60519e24.svg"
+  },
+  {
+    "revision": "4b9960c0f66aff1e48df233e2b44b6df",
+    "url": "/jsrpg/static/media/spiral-thrust.4b9960c0.svg"
+  },
+  {
+    "revision": "ad33f46f60f7da43901059fbe8a11927",
+    "url": "/jsrpg/static/media/stiletto.ad33f46f.svg"
+  },
+  {
+    "revision": "df43fabd8f9962dde41d6b0d0b71db78",
+    "url": "/jsrpg/static/media/swap-bag.df43fabd.svg"
+  },
+  {
+    "revision": "a3fe9ae2ba048a78883f5ab04fec8919",
+    "url": "/jsrpg/static/media/sword-brandish.a3fe9ae2.svg"
+  },
+  {
+    "revision": "409c650d7d09b634f20205080e28f900",
+    "url": "/jsrpg/static/media/sword-hilt.409c650d.svg"
+  },
+  {
+    "revision": "c84cfcb8106c70d2c5895b07a8bf3343",
+    "url": "/jsrpg/static/media/swordman.c84cfcb8.svg"
+  },
+  {
+    "revision": "bb5d2de7909383639e5228b9c3d16905",
+    "url": "/jsrpg/static/media/thunderbolt.bb5d2de7.png"
+  },
+  {
+    "revision": "31b6aa475338b7d300a36a44466ae299",
+    "url": "/jsrpg/static/media/tombstone.31b6aa47.svg"
+  },
+  {
+    "revision": "3332c6a4dbbf9098f82a71491694bb46",
+    "url": "/jsrpg/static/media/trash-can.3332c6a4.svg"
+  },
+  {
+    "revision": "b6fc9ecd396dc25ab7d39e11c651e5bf",
+    "url": "/jsrpg/static/media/triorb.b6fc9ecd.svg"
+  },
+  {
+    "revision": "3e048aca2039671b84a94e5a5f7c995f",
+    "url": "/jsrpg/static/media/two-shadows.3e048aca.svg"
+  },
+  {
+    "revision": "670b9e647b7cff92ade604b8c79cef89",
+    "url": "/jsrpg/static/media/vanish.670b9e64.png"
+  },
+  {
+    "revision": "e2df895654f2c2365a453306d16b1d8d",
+    "url": "/jsrpg/static/media/war-axe.e2df8956.svg"
+  },
+  {
+    "revision": "1f0b2b4e42e7b9705f8e8b9352a8c4d2",
+    "url": "/jsrpg/static/media/weapon-attack.1f0b2b4e.png"
+  },
+  {
+    "revision": "c22831aa4b38cd35b6cbe809a5a7b38c",
+    "url": "/jsrpg/static/media/winged-sword.c22831aa.svg"
+  },
+  {
+    "revision": "3e6091b9903623c1793701c92f4c4bd7",
+    "url": "/jsrpg/static/media/wizard-staff.3e6091b9.svg"
+  },
+  {
+    "revision": "b68f4b08a5ac0e7cc0b06a14e0c080e7",
+    "url": "/jsrpg/static/media/wrath.b68f4b08.png"
+  }
+]);
